20a139553ecf75846f6e8e6f66798b7503af287f
David <david@qa.com>
2025-04-08 14:13:33 +0100

    Changed the message

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -8,21 +8,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(sayBattle());
+        System.out.println(sayHello());
         System.out.println(sayPokemon("Pikachu"));
         System.out.println(sayPokemon("Gengar"));
         System.out.println(sayPokemon("Psyduck"));
         System.out.println(sayPokemon("Slowpoke"));
         System.out.println(sayPokemon("Growlithe"));
         System.out.println(sayPokemon("Charizard"));
-        System.out.println(sayWinner());
+        System.out.println(sayGoodbye());
     }
 
-    public static String sayBattle(){
+    public static String sayHello(){
         return "Hello Trainer.  Let's battle!";
     }
 
-    public static String sayWinner(){
+    public static String sayGoodbye(){
         return "I am the pokemon master!";
     }
 
